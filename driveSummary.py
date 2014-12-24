ce778f289c4569b958665347d72395bc75fea252
henryborchers@yahoo.com <B0rch3r5>
2014-12-23 17:01:07 -0800

    Changed report function to show the file type banner only if that type of file exists on the drive.

--- driveSummary.py
+++ driveSummary.py
@@ -17,57 +17,61 @@ def report(data, other_files):
     for other_file in other_files:
         print other_file
     print "\\=========================== Non-AV files on drive: END ===========================/"
-    print "\n=================== AUDIO FILES ==================="
-    if data["mp3_counter"] != 0:
-        print "The number of mp3 files are: \t\t" + str(data["mp3_counter"])
+    if data["audio_counter"] != 0:
+        print "\n=================== AUDIO FILES ==================="
+        if data["mp3_counter"] != 0:
+            print "The number of mp3 files are: \t\t" + str(data["mp3_counter"])
 
-    if data["wave_counter"] != 0:
-        print "The number of wav files are: \t\t" + str(data["wave_counter"])
+        if data["wave_counter"] != 0:
+            print "The number of wav files are: \t\t" + str(data["wave_counter"])
+    if data["video_counter"] != 0:
+        print "\n=================== VIDEO FILES ==================="
+        if data["avi_counter"] != 0:
+            print "The number of avi files are: \t\t" + str(data["avi_counter"])
 
-    print "\n=================== VIDEO FILES ==================="
-    if data["avi_counter"] != 0:
-        print "The number of avi files are: \t\t" + str(data["avi_counter"])
+        if data["dv_counter"] != 0:
+            print "The number of dv files are: \t\t" + str(data["dv_counter"])
 
-    if data["dv_counter"] != 0:
-        print "The number of dv files are: \t\t" + str(data["dv_counter"])
+        if data["mov_counter"] != 0:
+            print "The number of mov files are: \t\t" + str(data["mov_counter"])
 
-    if data["mov_counter"] != 0:
-        print "The number of mov files are: \t\t" + str(data["mov_counter"])
+        if data["mp4_counter"] != 0:
+            print "The number of mp4 files are: \t\t" + str(data["mp4_counter"])
 
-    if data["mp4_counter"] != 0:
-        print "The number of mp4 files are: \t\t" + str(data["mp4_counter"])
+        if data["m4v_counter"] != 0:
+            print "The number of m4v files are: \t\t" + str(data["m4v_counter"])
+    if data["image_counter"] != 0:
+        print "\n=================== IMAGE FILES ==================="
+        if data["tiff_counter"] != 0:
+            print "The number of tif files are: \t\t" + str(data["tiff_counter"])
 
-    if data["m4v_counter"] != 0:
-        print "The number of m4v files are: \t\t" + str(data["m4v_counter"])
+        if data["jpeg_counter"] != 0:
+            print "The number of jpg files are: \t\t" + str(data["jpeg_counter"])
 
-    print "\n=================== IMAGE FILES ==================="
-    if data["tiff_counter"] != 0:
-        print "The number of tif files are: \t\t" + str(data["tiff_counter"])
+        if data["bmp_counter"] != 0:
+            print "The number of bmp files are: \t\t" + str(data["bmp_counter"])
 
-    if data["jpeg_counter"] != 0:
-        print "The number of jpg files are: \t\t" + str(data["jpeg_counter"])
+        if data["gif_counter"] != 0:
+            print "The number of gif files are: \t\t" + str(data["gif_counter"])
 
-    if data["bmp_counter"] != 0:
-        print "The number of bmp files are: \t\t" + str(data["bmp_counter"])
+        if data["png_counter"] != 0:
+            print "The number of png files are: \t\t" + str(data["png_counter"])
+    if data["document_counter"] != 0:
+        print "\n================= DOCUMENT FILES =================="
+        if data["excel_counter"] != 0:
+            print "The number of excel files are: \t\t" + str(data["excel_counter"])
 
-    if data["gif_counter"] != 0:
-        print "The number of gif files are: \t\t" + str(data["gif_counter"])
+        if data["rtf_counter"] != 0:
+            print "The number of rich text \"rtf\" files are:" + str(data["rtf_counter"])
 
-    if data["png_counter"] != 0:
-        print "The number of png files are: \t\t" + str(data["png_counter"])
+        if data["txt_counter"] != 0:
+            print "The number of txt files are:\t\t" + str(data["txt_counter"])
 
-    print "\n================= DOCUMENT FILES =================="
-    if data["excel_counter"] != 0:
-        print "The number of excel files are: \t\t" + str(data["excel_counter"])
+        if data["doc_counter"] != 0:
+            print "The number of doc files are: \t\t" + str(data["doc_counter"])
 
-    if data["rtf_counter"] != 0:
-        print "The number of rich text \"rtf\" files are:" + str(data["rtf_counter"])
-
-    if data["doc_counter"] != 0:
-        print "The number of doc files are: \t\t" + str(data["doc_counter"])
-
-    if data["pdf_counter"] != 0:
-        print "The number of pdf files are: \t\t" + str(data["pdf_counter"])
+        if data["pdf_counter"] != 0:
+            print "The number of pdf files are: \t\t" + str(data["pdf_counter"])
 
     print "\n===================== SUMMARY ====================="
     print "The number of video files are: \t\t" + str(data["video_counter"])
@@ -98,6 +102,7 @@ def get_data(input_argument):
         "png_counter": 0,
         "excel_counter": 0,
         "rtf_counter": 0,
+        "txt_counter": 0,
         "doc_counter": 0,
         "pdf_counter": 0,
         "audio_counter": 0,
@@ -109,7 +114,7 @@ def get_data(input_argument):
         "total_counter": 0,
         "total_file_size": 0
     }
-    otherFiles = []
+    others_file_types = []
     if path.isdir(input_argument):
         for root, dir, files in walk(input_argument):
             for file in files:
@@ -189,6 +194,11 @@ def get_data(input_argument):
                         data["document_counter"] += 1
                         continue
 
+                    elif extension.lower() == ".txt":
+                        data["txt_counter"] += 1
+                        data["document_counter"] += 1
+                        continue
+
                     elif extension.lower() == ".rtf":
                         data["rtf_counter"] += 1
                         data["document_counter"] += 1
@@ -205,8 +215,8 @@ def get_data(input_argument):
 
                     else:
                         data["other_counter"] += 1
-                        otherFiles.append(path.join(root, file))
-    return otherFiles, data
+                        others_file_types.append(path.join(root, file))
+    return others_file_types, data
 
 
 def main():
