abc244b261db76455f0ea14f491e8d3a7c76f4a4
henryborchers <henryborchers@yahoo.com>
2015-01-07 11:25:14 -0800

    Fixed crash bug on clearing the QTreeWidget  by changing how data is added to the stats QTreeWidget. It's now directly added instead of going through a list first and added afterwards.

--- bin/driveSummaryGUI.py
+++ bin/driveSummaryGUI.py
@@ -40,98 +40,98 @@ class main_window(QMainWindow, gui.Ui_MainWindow):
         # print self.summary_item.data(0,1) + "dgdg"
 
     def populate_searched_for_files(self):
-        # TODO fix populate searched filee. Possibly Make each QTreeWidgetItem a member.
+        # TODO fix populate searched file. Possibly Make each QTreeWidgetItem a member.
         if not self.stats_tree.isEnabled():
             self.stats_tree.setEnabled(True)
         if self.data["audio_counter"] != 0:
 
             if self.data["mp3_counter"] != 0:
-                self.stat_items.append(QTreeWidgetItem(["Audio",
+                self.stats_tree.addTopLevelItem(QTreeWidgetItem(["Audio",
                                                         "MPEG-2 Audio Layer III",
                                                         ".mp3",
                                                         str(self.data["mp3_counter"])]))
 
             if self.data["m4a_counter"] != 0:
-                self.stat_items.append(QTreeWidgetItem(["Audio",
+                self.stats_tree.addTopLevelItem(QTreeWidgetItem(["Audio",
                                                         "",
                                                         ".m4a",
                                                         str(self.data["m4a_counter"])]))
 
             if self.data["mpc_counter"] != 0:
-                self.stat_items.append(QTreeWidgetItem(["Audio",
+                self.stats_tree.addTopLevelItem(QTreeWidgetItem(["Audio",
                                                         "Musepack",
                                                         ".mpc",
                                                         str(self.data["mpc_counter"])]))
 
             if self.data["wave_counter"] != 0:
-                self.stat_items.append(QTreeWidgetItem(["Audio",
+                self.stats_tree.addTopLevelItem(QTreeWidgetItem(["Audio",
                                                         "Waveform Audio File",
                                                         ".wav",
                                                         str(self.data["wave_counter"])]))
 
             if self.data["flac_counter"] != 0:
-                self.stat_items.append(QTreeWidgetItem(["Audio",
+                self.stats_tree.addTopLevelItem(QTreeWidgetItem(["Audio",
                                                         "Free Lossless Audio Codec",
                                                         ".flac",
                                                         str(self.data["flac_counter"])]))
 
             if self.data["ogg_counter"] != 0:
-                self.stat_items.append(QTreeWidgetItem(["Audio",
+                self.stats_tree.addTopLevelItem(QTreeWidgetItem(["Audio",
                                                         "Ogg",
                                                         ".ogg",
                                                         str(self.data["ogg_counter"])]))
 
             if self.data["wma_counter"] != 0:
-                self.stat_items.append(QTreeWidgetItem(["Audio",
+                self.stats_tree.addTopLevelItem(QTreeWidgetItem(["Audio",
                                                         "Windows Media Audio",
                                                         ".wma",
                                                         str(self.data["wma_counter"])]))
         if self.data["video_counter"] != 0:
             if self.data["avi_counter"] != 0:
-                self.stat_items.append(QTreeWidgetItem(["Video",
+                self.stats_tree.addTopLevelItem(QTreeWidgetItem(["Video",
                                                         "Audio Video Interleave",
                                                         ".avi",
                                                         str(self.data["avi_counter"])]))
 
             if self.data["dv_counter"] != 0:
-                self.stat_items.append(QTreeWidgetItem(["Video",
+                self.stats_tree.addTopLevelItem(QTreeWidgetItem(["Video",
                                                         "DV",
                                                         ".dv",
                                                         str(self.data["dv_counter"])]))
 
             if self.data["mov_counter"] != 0:
-                self.stat_items.append(QTreeWidgetItem(["Video",
+                self.stats_tree.addTopLevelItem(QTreeWidgetItem(["Video",
                                                         "QuickTime",
                                                         ".mov",
                                                         str(self.data["mov_counter"])]))
 
             if self.data["mp4_counter"] != 0:
-                self.stat_items.append(QTreeWidgetItem(["Video",
+                self.stats_tree.addTopLevelItem(QTreeWidgetItem(["Video",
                                                         "MPEG-4 Part 14",
                                                         ".mp4",
                                                         str(self.data["mp4_counter"])]))
 
             if self.data["m4v_counter"] != 0:
-                self.stat_items.append(QTreeWidgetItem(["Video",
+                self.stats_tree.addTopLevelItem(QTreeWidgetItem(["Video",
                                                         "M4V",
                                                         ".m4v",
                                                         str(self.data["m4v_counter"])]))
 
         if self.data["image_counter"] != 0:
             if self.data["tiff_counter"] != 0:
-                self.stat_items.append(QTreeWidgetItem(["Image",
+                self.stats_tree.addTopLevelItem(QTreeWidgetItem(["Image",
                                                         "Tagged Image File Format",
                                                         ".tif",
                                                         str(self.data["tiff_counter"])]))
 
             if self.data["jpeg_counter"] != 0:
-                self.stat_items.append(QTreeWidgetItem(["Image",
+                self.stats_tree.addTopLevelItem(QTreeWidgetItem(["Image",
                                                         "JPEG",
                                                         ".jpg",
                                                         str(self.data["jpeg_counter"])]))
 
             if self.data["bmp_counter"] != 0:
-                self.stat_items.append(QTreeWidgetItem(["Image",
+                self.stats_tree.addTopLevelItem(QTreeWidgetItem(["Image",
                                                         "BMP",
                                                         ".bmp",
                                                         str(self.data["bmp_counter"])]))
@@ -143,46 +143,46 @@ class main_window(QMainWindow, gui.Ui_MainWindow):
                                                         str(self.data["gif_counter"])]))
 
             if self.data["png_counter"] != 0:
-                self.stat_items.append(QTreeWidgetItem(["Image",
+                self.stats_tree.addTopLevelItem(QTreeWidgetItem(["Image",
                                                         "Portable Network Graphics",
                                                         ".png",
                                                         str(self.data["png_counter"])]))
 
         if self.data["document_counter"] != 0:
             if self.data["excel_counter"] != 0:
-                self.stat_items.append(QTreeWidgetItem(["Document",
+                self.stats_tree.addTopLevelItem(QTreeWidgetItem(["Document",
                                                         "Excel",
                                                         ".xlsx or .xls",
                                                         str(self.data["excel_counter"])]))
 
             if self.data["rtf_counter"] != 0:
-                self.stat_items.append(QTreeWidgetItem(["Document",
+                self.stats_tree.addTopLevelItem(QTreeWidgetItem(["Document",
                                                         "Rich Text Format",
                                                         ".rtf",
                                                         str(self.data["rtf_counter"])]))
 
             if self.data["txt_counter"] != 0:
-                self.stat_items.append(QTreeWidgetItem(["Document",
+                self.stats_tree.addTopLevelItem(QTreeWidgetItem(["Document",
                                                         "Text",
                                                         ".txt",
                                                         str(self.data["txt_counter"])]))
 
             if self.data["doc_counter"] != 0:
-                self.stat_items.append(QTreeWidgetItem(["Document",
+                self.stats_tree.addTopLevelItem(QTreeWidgetItem(["Document",
                                                         "Doc",
                                                         ".doc",
                                                         str(self.data["doc_counter"])]))
 
             if self.data["pdf_counter"] != 0:
-                self.stat_items.append(QTreeWidgetItem(["Document",
+                self.stats_tree.addTopLevelItem(QTreeWidgetItem(["Document",
                                                         "Portable Document Format",
                                                         ".pdf",
                                                         str(self.data["pdf_counter"])]))
-            self.stats_tree.addTopLevelItems(self.stat_items)
+            # self.stats_tree.addTopLevelItems(self.stat_items)
 
     def clear_old_data(self):
-        # TODO: fix clear bug (http://stackoverflow.com/questions/22934043/pyside-qtreewidget-clear-causes-crash)
-        # self.stats_tree.clear()
+        self.stats_tree.clear()
+        # self.stats_tree.rowsRemoved()
         self.other_files_list.clear()
 
     def _populate_summary_stats(self):
