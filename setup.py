47dbb264bcf221d7128416c3d0f3a285ed4fe0b2
henryborchers <henryborchers@yahoo.com>
2015-01-07 13:45:42 -0800

    added setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
-from distutils.core import setup
-
+# from distutils.core import setup
+from setuptools import setup
 setup(
     name='driveSummary',
-    version='0.1.0',
-    scripts=['bin/driveSummary.py'],
+    version='0.1.1',
     url='https://github.com/cavpp/drive_summary',
     license='MIT',
     author='Henry Borchers',
     author_email='hborcher@berkeley.edu',
     description='Written for the California AV Preservation Project, Utility to get a summary of the AV files on a '
-                'given drive or folder'
+                'given drive or folder',
+    include_package_data=True,
+    scripts=['bin/driveSummaryCLI.py', "bin/driveSummaryGUI.py"],
+    packages=['driveSummary'],
+    install_requires=['pip >= 6.0',
+                      'setuptools >= 11.3',
+                      'PySide >= 1.2.2']
 )
